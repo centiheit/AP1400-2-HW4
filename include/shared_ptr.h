84c891ca9f94e7e6efcffad564977c6c809fd1e4
czh <chriscuizh@foxmail.com>
2024-09-16 15:13:28 +0800

    Finished by czh

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -1,5 +1,124 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
 
+template <typename T>
+class SharedPtr {
+public:
+    // 赋值构造函数
+    SharedPtr(T* ptr);
+    // 默认构造函数
+    SharedPtr();
+    // 析构函数
+    ~SharedPtr();
+    // 拷贝构造函数
+    SharedPtr(const SharedPtr<T>& ptr);
+
+    // 重载赋值运算符
+    SharedPtr<T>& operator=(const SharedPtr<T>& ptr);
+    // 引用计数器
+    int use_count();
+
+    T* get();
+
+    // 重载解引用运算符
+    T& operator*();
+    // 重载指向运算符
+    T* operator->();
+    // 复位
+    void reset();
+    void reset(T* ptr);
+
+    explicit operator bool() const;
+
+private:
+    T* _p;
+    size_t* _count;
+};
+
+template <typename T>
+SharedPtr<T>::SharedPtr(T* ptr) : _p(ptr) {
+    _count = new size_t(1);
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr() : _p(nullptr) {
+    _count = new size_t(1);
+}
+
+template <typename T>
+SharedPtr<T>::~SharedPtr() {
+    // 这里我没有释放_count，因为TEST15中还需要用到_count
+    *_count = *_count - 1;
+    if (*_count == 0) {
+        delete _p;
+    }
+    _p = nullptr;
+}
+
+// 这里实现的是浅拷贝
+template <typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr<T>& ptr) {
+    _p = ptr._p;
+    _count = ptr._count;
+    *_count = *_count + 1;
+}
+
+template <typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& ptr) {
+    if (this != &ptr) {
+        reset();
+        _p = ptr._p;
+        _count = ptr._count;
+        *_count = *_count + 1;
+    }
+    return *this;
+}
+
+template <typename T>
+int SharedPtr<T>::use_count() {
+    return *_count;
+}
+
+template <typename T>
+T* SharedPtr<T>::get() {
+    return _p;
+}
+
+template <typename T>
+T& SharedPtr<T>::operator*() {
+    return *_p;
+}
+
+template <typename T>
+T* SharedPtr<T>::operator->() {
+    return _p;
+}
+
+template <typename T>
+void SharedPtr<T>::reset() {
+    *_count = *_count - 1;
+    if (*_count <= 0) {
+        delete _p;
+    }
+    _p = nullptr;
+}
+
+template <typename T>
+void SharedPtr<T>::reset(T* ptr) {
+    reset();
+    _p = ptr;
+    _count = new size_t(1);
+}
+
+template <typename T>
+SharedPtr<T>::operator bool() const {
+    return _p != nullptr;
+}
+
+template <typename T>
+SharedPtr<T> make_shared(T data) {
+    T* ptr = new T(data);
+    return SharedPtr<T>(ptr);
+}
 
 #endif //SHARED_PTR
--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,14 +1,97 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
 
+template <typename T>
+class UniquePtr {
+public:
+    // 赋值构造函数
+    UniquePtr(T* ptr);
+    // 默认构造函数
+    UniquePtr();
+    // 析构函数
+    ~UniquePtr();
+    // 禁用拷贝构造函数
+    // delete 关键字用于删除函数，使得该函数不能被调用
+    UniquePtr(const UniquePtr<T>& ptr) = delete;
+    // 禁用拷贝赋值运算符
+    UniquePtr<T>& operator=(const UniquePtr<T>& ptr) = delete;
 
+    T* get();
 
+    // 解引用运算符
+    T& operator*();
+    // 指向运算符
+    T* operator->();
+    // 复位
+    void reset();
+    void reset(T* ptr);
+    // 释放
+    T* release();
 
+    // 隐式转换（Implicit Conversion）：在不使用显式关键字的情况下，编译器可以在需要的时候自动调用转换运算符。
+    // 例如，类类型对象在需要转换为布尔值或整数时，编译器会自动使用这个转换运算符进行类型转换。
+    // 显式转换（Explicit Conversion）：在C++11引入的explicit关键字用于防止隐式类型转换，
+    // 仅允许在明确需要的时候（如在static_cast或if语句中）使用。这样可以避免意外的类型转换导致的错误。
+    explicit operator bool() const;
 
+private:
+    T* _p;
+};
 
+template <typename T>
+UniquePtr<T>::UniquePtr(T* ptr) : _p(ptr) {}
 
+template <typename T>
+UniquePtr<T>::UniquePtr() : _p(nullptr) {}
 
+template <typename T>
+UniquePtr<T>::~UniquePtr() {
+    delete _p;
+}
 
+template <typename T>
+T* UniquePtr<T>::get() {
+    return _p;
+}
 
+template <typename T>
+T& UniquePtr<T>::operator*() {
+    return *_p;
+}
+
+template <typename T>
+T* UniquePtr<T>::operator->() {
+    return _p;
+}
+
+template <typename T>
+void UniquePtr<T>::reset() {
+    delete _p;
+    _p = nullptr;
+}
+
+template <typename T>
+void UniquePtr<T>::reset(T* ptr) {
+    delete _p;
+    _p = ptr;
+}
+
+template <typename T>
+T* UniquePtr<T>::release() {
+    T* tmp = _p;
+    _p = nullptr;
+    return tmp;
+}
+
+template <typename T>
+UniquePtr<T>::operator bool() const {
+    return _p != nullptr;
+}
+
+template <typename T>
+UniquePtr<T> make_unique(T data) {
+    T* ptr = new T(data);
+    return UniquePtr<T>(ptr);
+}
 
 #endif //UNIQUE_PTR
